d25074e8b51fd2fd4809b889912d354965a9ab19
Spidx App <administrative@spidx.app>
2022-04-01 09:53:07 -0300

    changed port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const express = require('express')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const request = require('request');
 
 
